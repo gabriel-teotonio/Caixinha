5e992ece4e8cbf8a501bc4f2a220ef264138f7e4
Gabriel silva <gabriel23teotonio@gmail.com>
2023-05-24 13:23:30 -0300

    feat: add handleSubmitUserForm function \ remove the react-input-mask

--- src/Components/Forms/FormCreateUser/index.tsx
+++ src/Components/Forms/FormCreateUser/index.tsx
@@ -1,26 +1,47 @@
+import { useState } from 'react';
 import * as C from './styles';
-
-const PhoneInput = () => {
-    return (
-      <C.InputMask
-      mask="(99) 99999-9999"
-      placeholder='(91) 98283-8485'
-      />
-    )
-}
+import { IUser } from '../../../types/users';
 
 export const FormCreateUser = () => {
+  const [name, setName] = useState("")
+  const [phone, setPhone] = useState("")
+
+  const handleSubmitUserForm = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if(name === "") return console.log("preencha o campo nome")
+    if(phone === "") return console.log("preencha o campo telefone")
+    
+    console.log(name,phone)
+    setName("")
+    setPhone("")
+  }
+  
   return (
     <C.Container>
         <C.FieldBox>
             <C.Label>Nome do usuário</C.Label>
-            <C.Input type='text' placeholder='Ex: Gabriel Teotonio'/>
+            <C.Input
+            type='text' 
+            placeholder='Ex: Gabriel Teotonio'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            />
         </C.FieldBox>
         <C.FieldBox>
             <C.Label>Número de telefone</C.Label>
-            <PhoneInput />
+            <C.Input 
+            type='text' 
+            placeholder='(91) 96666-7777'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            />
         </C.FieldBox>
-        <C.ButtonSubmit type='submit'>Criar</C.ButtonSubmit>
+        <C.ButtonSubmit
+        type='submit'
+        onClick={handleSubmitUserForm}
+        >
+        Criar
+         </C.ButtonSubmit>
     </C.Container>
   )
 }
