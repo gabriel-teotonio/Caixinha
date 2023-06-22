91c781f8b36e5ef66d547280cdcf684134858e7d
Gabriel silva <gabriel23teotonio@gmail.com>
2023-06-21 21:47:31 -0300

    feat: add dynamic title in header and back button

--- src/Components/HeaderMain/index.tsx
+++ src/Components/HeaderMain/index.tsx
@@ -3,16 +3,49 @@ import { ActionButton } from '../Buttons/ActionButton';
 import * as C from './styles';
 import { useModalContext } from '../../contexts/modalContext';
 import { CreationModal } from '../CreationModal';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../contexts/appContext';
 
 export const HeaderMain = () => {
   const {openModal} = useModalContext()
+  const {isMobileScreen} = useAppContext()
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const handleBack = () => {
+    navigate(-1)
+  }
+
+  const getTitle = (pathname: string) => {
+    switch (pathname) {
+      case "/":
+        return "Olá, Gabriel!";
+      case "/users":
+        return "Usuários";
+      case "/transactions":
+        return "Transações";
+      default:
+        if(pathname.startsWith("/users/")){
+          return "Detalhes do usuário"
+        }
+        return "";
+    }
+  };
   return (
     <C.Container>
-        <C.TitleHeaderMain>Olá, Gabriel! <br /> </C.TitleHeaderMain>
-        <ActionButton 
-        icon={<MdFormatListBulletedAdd />}
-        action={() => openModal(<CreationModal />)}
-        />
+        <C.TitleHeaderMain>{getTitle(location.pathname)}</C.TitleHeaderMain>
+
+        <C.BtnGroup>
+          {
+            !isMobileScreen ? (<ActionButton 
+            icon={<MdFormatListBulletedAdd />}
+            action={() => openModal(<CreationModal />)}
+            />) : null
+          }
+          {
+            location.pathname !== "/" && <button onClick={handleBack}>VOLTAR</button>  
+          }
+        </C.BtnGroup> 
     </C.Container>
   )
 }
--- src/Components/HeaderMain/styles.tsx
+++ src/Components/HeaderMain/styles.tsx
@@ -6,9 +6,18 @@ export const Container = styled.header`
     justify-content: space-between;
     align-items: center;
     color: ${props => props.theme.colors.darkBlue};
-`
+    `
 
 export const TitleHeaderMain = styled.h2`
     font-weight: ${props => props.theme.fontWeight.medium};
     font-size: ${props => props.theme.fontSizes.xl};
+    
+    @media (max-width: 425px) {
+        font-size: ${props => props.theme.fontSizes.lg};
+    }  
+    `
+export const BtnGroup = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 `
